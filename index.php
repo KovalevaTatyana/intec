249322a85d4a738a7d62e44a73620e1448f85ded
Kovaleva Tatyana <kovaleva.tatyana@protonmail.com>
2021-11-28 15:19:14 +0500

    add form

---
+++ index.php
@@ -0,0 +1,4 @@
+<form action="import.php" method="post">
+    <input type="file" name="sql-file">
+    <input type="submit" value="Загрузить">
+</form>
---
+++ init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE productsdb;
+USE productsdb;
+CREATE TABLE users
+(
+    id INT PRIMARY KEY AUTO_INCREMENT
+);
+CREATE TABLE products
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) UNIQUE,
+    code INT UNIQUE,
+    price FLOAT,
+    preview_text TEXT(30),
+    detail_text TEXT,
+    user_id INT ,
+    FOREIGN KEY (user_id)  REFERENCES users(id) ON DELETE CASCADE
+);
+
