d9b9b129958bc1bb602b9eb6e043bf07c5aa1696
Kovaleva Tatyana <kovaleva.tatyana@protonmail.com>
2021-11-28 15:55:34 +0500

    output csv file content

--- import.php
+++ import.php
@@ -1,3 +1,17 @@
 <?php
-
+// print_r($_FILES['csv-file']);
+// print_r($_FILES['csv-file']['full_path']);
+// print_r($_FILES['csv-file']['tmp_name']);
+$row = 1;
+if (($handle = fopen($_FILES['csv-file']['tmp_name'], "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num полей в строке $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form action="import.php" method="post">
-    <input type="file" name="sql-file">
+<form enctype="multipart/form-data" action="import.php" method="post">
+    <input type="file" name="csv-file">
     <input type="submit" value="Загрузить">
 </form>
