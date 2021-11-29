7bed9a63967e973d30cb49db92760cf12c3cc5c8
Kovaleva Tatyana <kovaleva.tatyana@protonmail.com>
2021-11-29 17:33:35 +0500

    add mysql injection

--- import.php
+++ import.php
@@ -6,7 +6,9 @@ $mysqli = new mysqli("127.0.0.1", "root", "135798642", "productsdb", 3306);
 if (($handle = fopen($_FILES['csv-file']['tmp_name'], "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
        if ($session_user_id == $data[5]) {
-            $mysqli->query("INSERT INTO products (`name`, code, price, preview_text, detail_text, user_id) VALUES('$data[0]','$data[1]','$data[2]','$data[3]', '$data[4]', '$data[5]')");
+            $stmt = $mysqli->prepare("INSERT INTO products (`name`, code, price, preview_text, detail_text, user_id) VALUES(?,?,?,?,?,?)");
+            $stmt->bind_param('siissi', $data[0],$data[1],$data[2],$data[3], $data[4],$data[5]);
+            $stmt->execute();
         }
     }
     fclose($handle);
