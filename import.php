976318177ae68afa9f72aaa4aa62914b20fa4202
Kovaleva Tatyana <kovaleva.tatyana@protonmail.com>
2021-11-30 09:51:02 +0500

    add counted strings

--- import.php
+++ import.php
@@ -3,14 +3,17 @@ $user_ids = array(1, 2, 3);
 $user_id_key = array_rand($user_ids, 1);
 $session_user_id = $user_ids[$user_id_key];
 $mysqli = new mysqli("127.0.0.1", "root", "135798642", "productsdb", 3306);
+$row = 0;
 if (($handle = fopen($_FILES['csv-file']['tmp_name'], "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
        if ($session_user_id == $data[5]) {
             $stmt = $mysqli->prepare("INSERT INTO products (`name`, code, price, preview_text, detail_text, user_id) VALUES(?,?,?,?,?,?)");
             $stmt->bind_param('siissi', $data[0],$data[1],$data[2],$data[3], $data[4],$data[5]);
             $stmt->execute();
+            $row++;
         }
     }
     fclose($handle);
+    echo 'добавлено ' . $row;
 }
 ?>
